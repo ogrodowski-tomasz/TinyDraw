805af1b38a72be911902857913f897d6f493a0c4
Tomasz Ogrodowski <tomek@MacBook-Pro-Tomasz.local>
2022-05-09 13:51:52 +0200

    first commit

---
+++ TinyDraw/TinyDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyDrawApp.swift
+//  TinyDraw
+//
+//  Created by Tomasz Ogrodowski on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TinyDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
